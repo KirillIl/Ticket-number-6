59e4b8348aea0da3c680bce72bd43435aea36bc0
Admin <ilinskiy_kirill@mail.ru>
2022-11-14 00:19:58 +0300

    Билет № 6 прошу принять

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+    /*Необходимо реализовать следующий метод
+         1. Получаем на входе массив чисел.
+         2. Все четные числа увеличиваем на единицу.
+         3. Возвращаем кусок списка с 3-го по 7-й элемент.
+         */
+    public static int enterMassiveNumber( int[] numbers){
+        int i = 0;
+        for (i = 0; i < numbers.length - 1; i++){
+            if (numbers[i] % 2 == 0) {
+                numbers[i] += 1;
+            }
+            for (i=2; i<7; i++){
+                System.out.println("элементы " + i);
+            }
+        }
+        return numbers[1];
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] ululuha = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("" + enterMassiveNumber(ululuha));
     }
 }
